f5ba8ba9b5f55fd46671b2c53e8ac016d8e4f849
jmg0137 <32061421+jmg0137@users.noreply.github.com>
2017-09-25 23:14:12 +0200

    Cambio en la base de datos
    Cambio en los campos de la tabla usuarios

--- WebPage/MyApp/sql_scripts/schema.sql
+++ WebPage/MyApp/sql_scripts/schema.sql
@@ -2,5 +2,8 @@
 drop table if exists users;
 
 create table users (
-    email text
+    email text CONSTRAINT pk_email PRIMARY KEY,
+	name text NOT NULL,
+	rol text DEFAULT 'USER',
+	CONSTRAINT CHK_Rol CHECK (rol='ADMIN' OR rol='USER')
 );
