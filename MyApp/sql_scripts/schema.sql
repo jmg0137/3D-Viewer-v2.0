b58268a9bea34adca367a51f9f9b1ab2598644ad
jmg0137 <32061421+jmg0137@users.noreply.github.com>
2017-10-26 23:42:34 +0200

    Eliminación del campo rol de la base de datos

--- MyApp/sql_scripts/schema.sql
+++ MyApp/sql_scripts/schema.sql
@@ -3,7 +3,5 @@ drop table if exists users;
 
 create table users (
     email text CONSTRAINT pk_email PRIMARY KEY,
-	name text NOT NULL,
-	rol text DEFAULT 'USER',
-	CONSTRAINT CHK_Rol CHECK (rol='ADMIN' OR rol='USER')
+	name text NOT NULL
 );
