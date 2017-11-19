9a7e23089eccc7ad1433e9080cec4e38406e95c1
jmg0137 <32061421+jmg0137@users.noreply.github.com>
2017-11-19 18:13:12 +0100

    Inclusión botón que guarda los datos

--- src/PointManager.js
+++ src/PointManager.js
@@ -18,6 +18,8 @@ export class PointManager {
         this.measurements = measurements;
         this.__initImportButton();
         this.__initExportButton();
+        this.__initSavetButton();
+        this.__initLoadtButton();
     }
 
     /**
@@ -103,5 +105,100 @@ export class PointManager {
             icon: "ui-icon-arrowthickstop-1-s",
             text: false
         });
+    }
+
+    /**
+     * Inits the save button on the menu.
+     */
+    __initSavetButton() {
+        let instance = this;
+        $( "#save-points").click(
+            function exportJSONPoints() {
+                let url = document.URL;
+                let documentFilename = url.substring(url.lastIndexOf('/') + 1);
+                let annotations = instance.annotations.toJSON();
+                let measurements = instance.measurements.toJSON();
+                let json = Object.assign({"filename": documentFilename}, annotations, measurements);
+                $.ajax({
+                    url: '/_add_checksum_to_json',
+                    type: "POST",
+                    data: JSON.stringify(json),
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    success: 
+                        function(data){
+                            //let filename = "saveDataPoints.json";
+                            $.ajax({
+                                url: '/_save_json_in_my_folder',
+                                type: "POST",
+                                data: JSON.stringify(json),
+                                dataType: "json",
+                                contentType: "application/json; charset=utf-8",
+                                success: 
+                                    function (data) {
+                                        console.log("SUCCESS!!");
+                                    }
+                            });
+                        }
+                });
+            }
+        ).button({
+            icon: "ui-icon-document",
+            text: false
+        });
+    }
+
+    /**
+     * Inits the load button on the menu.
+     */
+    __initLoadtButton() {
+        let instance = this;
+        $( "#load-points").click(
+            function () {
+                $("#file").click();
+            }
+        ).button({
+            icon: "ui-icon-arrowthickstop-1-n",
+            text: false
+        });
+        $( "#file" ).change(
+            function () {
+                //It should only be one file.
+                let file = this.files[0];
+                let json = Utils.loadJSON(file,
+                    function(event){
+                        let text = event.target.result;
+                        let json = JSON.parse(text);
+
+                        //Warn when the file we are importing doesn't match the model.
+                        let url = document.URL;
+                        let documentFilename = url.substring(url.lastIndexOf('/') + 1);
+                        let jsonFilename = json.filename;
+                        if (jsonFilename !== documentFilename) {
+                            $( "#not-same-model-warn").dialog({
+                                title: "Warning!",
+                            });
+                        }
+
+                        //Load annotations.
+                        let annotations = json.annotations;
+                        for (let annotation of annotations) {
+                            let point = annotation.points[0];
+                            let tag = annotation.tag;
+                            instance.annotations.addPoint(point, tag);
+                        }
+                        //Load measurements.
+                        let measurements = json.measurements;
+                        for (let measurement of measurements) {
+                            let point1 = measurement.points[0];
+                            let point2 = measurement.points[1];
+                            let tag = measurement.tag;
+                            instance.measurements.addPoint(point1);
+                            instance.measurements.addPoint(point2, tag);
+                        }
+                    }
+                );
+            }
+        );
     }
 }
