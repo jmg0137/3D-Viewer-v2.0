46bcbbced45770f07a5145bd15ae0bcba19cf23b
jmg0137 <32061421+jmg0137@users.noreply.github.com>
2017-12-14 22:17:26 +0100

    Fichor de configuración de lanzamiento de nuestra aplicación en el servidor

---
+++ MyApp.wsgi.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.abspath('/var/www/3D-Viewer-v2.0/MyApp'))
+sys.path.append(os.path.abspath('/var/www/3D-Viewer-v2.0'))
+
+activate_this = '/var/www/3D-Viewer-v2.0/mi_venv/bin/activate_this.py'
+with open(activate_this) as file:
+    exec(file.read(), dict(__file__=activate_this))
+
+path = os.path.join(os.path.dirname(__file__), os.pardir)
+if path not in sys.path:
+    sys.path.append(path)
+
+from MyApp import APP as application
+
+if __name__ == '__main__':
+    APP.run()
